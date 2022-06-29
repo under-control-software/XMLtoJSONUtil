17c5455ba17d694cb102cad20428141709482e79
Shreetesh M <shreetesh.m@sprinklr.com>
2022-06-29 18:29:29 +0530

    handled error responses

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const fs = require("fs");
 const parser = require("xml2json");
 
-fs.readFile("/users/shreeteshm/Desktop/test.xml", "utf-8", (err, data) => {
+fs.readFile("/users/shreeteshm/Downloads/c4r1.xml", "utf-8", (err, data) => {
   if (err) {
     console.log("ERROR:", err);
     return;
   }
-
+  console.log("Running...");
   const jsonData = JSON.parse(parser.toJson(data));
   let innerData,
-    avgTime = 0;
+    avgTime = 0,
+    count = 0;
   jsonData.testResults.httpSample.forEach((value, ind) => {
-    innerData = JSON.parse(value.responseData["$t"]).data;
-    avgTime += parseInt(innerData.hero.queryTime);
+    try {
+      innerData = JSON.parse(value.responseData["$t"]).data;
+      count++;
+      if (parseInt(innerData.hero.queryTime) < 1) {
+        avgTime += 1;
+      } else {
+        avgTime += parseInt(innerData.hero.queryTime);
+      }
+    } catch (err) {}
   });
-  console.log(
-    "Average time:",
-    avgTime / jsonData.testResults.httpSample.length
-  );
+  console.log("Average time:", avgTime / count);
+  console.log(count);
 });
